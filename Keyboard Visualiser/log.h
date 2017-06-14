ce993bbd0669ae0131556c2a2b866edc11101eb0
Zain <zir500@york.ac.uk>
2017-06-14 16:04:27 +0100

    Minor Changes to logging macros
    Added newline after Handle Error messages,
    Added system pause after printing messages from EXIT_ON_ERROR

--- Keyboard Visualiser/log.h
+++ Keyboard Visualiser/log.h
@@ -4,6 +4,6 @@
 
 #define LOG(FORMAT, ...) wprintf(L"[  LOG  ] " FORMAT L"\n", __VA_ARGS__)
 #define ERR(FORMAT, ...) wprintf(L"[ ERROR ] " FORMAT L"\n", __VA_ARGS__)
-#define EHR(MESSAGE, HRES) wprintf(L"[ ERROR ] " MESSAGE L" HRESULT = 0x%08x", HRES)
+#define EHR(MESSAGE, HRES) wprintf(L"[ ERROR ] " MESSAGE L" HRESULT = 0x%08x\n", HRES)
 
-#define EXIT_ON_ERROR(message, hr) if(FAILED(hr)){EHR(message, hr); exit(hr);}
+#define EXIT_ON_ERROR(message, hr) if(FAILED(hr)){EHR(message, hr);system("pause"); exit(hr);}
